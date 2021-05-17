df7925307915378704766ec9bcb86ccf0dde2b68
Sam Singer <sam.singrt79@gmail.com>
2021-05-17 14:27:24 -0700

    mvp

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,28 +1,80 @@
-// Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
+const router = require("express").Router();
+const User = require("../users/users-model");
+const bcrypt = require("bcryptjs");
 
+router.post("/register", (req, res, next) => {
+  const { username, password } = req.body;
 
-/**
-  [GET] /api/users
+  const hash = bcrypt.hashSync(
+    password, // the string we're hashing
+    8 // 2 ^ 8 rounds of hashing
+  );
 
-  This endpoint is RESTRICTED: only authenticated clients
-  should have access.
+  User.add({ username, password: hash })
+    .then((user) => {
+      res.status(201).json({
+        message: `We are happy to have you, ${user.username}`,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+router.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
 
-  response:
-  status 200
-  [
-    {
-      "user_id": 1,
-      "username": "bob"
-    },
-    // etc
-  ]
-
-  response on non-authenticated:
-  status 401
-  {
-    "message": "You shall not pass!"
+  User.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // there should be a req.session we can write to
+        // if we change the req.session object
+        //  1- the session gets persisted
+        //  2- the response includes a SET-COOKIE with the session id
+        req.session.user = user;
+        res.json({
+          message: `Welcome back, ${user.username}`,
+        });
+      } else {
+        next({
+          status: 401,
+          message: `Invalid credentials`,
+        });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+router.get("/logout", (req, res, next) => {
+  if (req.session.user) {
+    const { username } = req.session.user;
+    req.session.destroy((err) => {
+      if (err) {
+        res.json({
+          message: `you can not leave, ${username}`,
+        });
+      } else {
+        // OPTIONALLY
+        // set a cookie manually 'monkey'
+        // with an expiration data set in the past
+        // and compliant browsers will wipe cookie
+        res.json({
+          message: `thanks for playing, ${username}`,
+        });
+      }
+    });
+  } else {
+    res.json({
+      message: "do I know you?",
+    });
   }
- */
+});
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessage: "something went wrong in auth router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
-
-// Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
